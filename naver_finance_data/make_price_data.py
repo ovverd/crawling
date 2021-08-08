bedfa34f19968a04d025d042adad8dd4b6e0281d
ovverd <88417854+ovverd@users.noreply.github.com>
2021-08-08 19:50:09 +0900

    Update make_price_data.py
    함수로 간결하게 변경

--- naver_finance_data/make_price_data.py
+++ naver_finance_data/make_price_data.py
@@ -18,10 +18,11 @@ def make_price_dataframe(code, startTime, endTime, name = '기업이름',  timef
     price_url = url + '&symbol=' + code + '&startTime=' + startTime + '&endTime=' + endTime + '&timeframe=' + timeframe
     price_data = requests.get(price_url)
     text_data = price_data.text
+
+    
     text_data = text_data.strip()
     text_data = text_data.split('\n\t\t\n')
     text_data[-2] = text_data[-2] + ','
-
 
     date_list = []
     open_list = []
@@ -31,9 +32,8 @@ def make_price_dataframe(code, startTime, endTime, name = '기업이름',  timef
     volume_list = []
     
     for row in text_data[1:-1]:
-        
-        # 데이터형식이 다른 것들이 포함되어있어서 추가
-        if row[-4] == ',':
+                
+        if row[-4] == ',':                  # 문자열 데이터가 각각 달라서 정제하는 코드
             row = row[:-4] + (']')
         else:
             row = row[:-1]
@@ -54,42 +54,67 @@ def make_price_dataframe(code, startTime, endTime, name = '기업이름',  timef
 
 
 
+
+def get_price_data(path, startDate,endDate,timeframe, number = 0):
+
+    
+    code_data = pd.read_excel(path)
+    code_data = code_data[['단축코드','한글 종목명']]
+    code_data['단축코드'] = code_data['단축코드'].apply(make_code)
+
+    if number == 0:
+        number = len(code_data)
         
+    for num, code, name in zip(range(1, number+1), code_data['단축코드'][:number+1], code_data['한글 종목명'][:number+1]):
 
-path = r'/Users/종목코드 데이터가 있는 엑셀파일 경로.xlsx'
-
-code_data = pd.read_excel(path)
-code_data = code_data[['단축코드','한글 종목명']]
-code_data['단축코드'] = code_data['단축코드'].apply(make_code)
-
-
-# 모든 종목 데이터 받으려면 zip( range(1, len(code_data)), code_data['단축코드'], code_data['한글 종목명'] 으로 수정
-
-for num, code, name in zip(range(1, 10), code_data['단축코드'][:10], code_data['한글 종목명'][:10]):
-
-    try:
-        print(num, code, name)
-        time.sleep(1)
         try:
-            price_df = make_price_dataframe(code, '20210803','20210803', name = name)
-        except requests.exceptions.Timeout:
-            time.sleep(60)
-            price_df = make_price_dataframe(code, name, '20210803','20210803', name = name)
-        except ConnectionResetError:
-            time.sleep(60)
-            price_df = make_price_dataframe(code, name, '19900101','20210807', name = name)
-        
-        if num == 1:
-            total_price = price_df
+            print(num, code, name)
+            time.sleep(1)
+            try:
+                price_df = make_price_dataframe(code, startDate, endDate, name = name, timeframe = timeframe)
             
-        else:
-            total_price = pd.merge(total_price, price_df, how='outer', right_index=True, left_index=True)
-    except ValueError:
-        continue
-    except KeyError:
-        continue
+            # 커넥션 에러 2가지 예외처리, 에러 났을 때 끊키지 않고 60초 후 재시도
+            except requests.exceptions.Timeout:
+                time.sleep(60)
+                price_df = make_price_dataframe(code, startDate, endDate, name = name, timeframe = timeframe)
+            except ConnectionResetError:
+                time.sleep(60)
+                price_df = make_price_dataframe(code, startDate, endDate, name = name, timeframe = timeframe)
+            
+            if num == 1:
+                total_price = price_df
+                
+            else:
+                total_price = pd.merge(total_price, price_df,how='outer', right_index=True, left_index=True)
+        except ValueError:
+            print( code, 'valueError')
+            continue
+        except KeyError:
+            print( code, 'KeyError')
+            continue
+    
+    # 인덱스에 '20210807'과 같은 문자열을 datetime 타입으로 변환하여 깔끔하게 저장
+    total_price.index = pd.to_datetime(total_price.index)
+    
+    return total_price
+        
+
+    
+    
+    
+    
+    
+# 종목코드 엑셀 경로
+path = r'/Users/종목코드 파일 경로.xlsx'
 
 
-total_price.index = pd.to_datetime(total_price.index)
-total_price.to_excel(r'/Users/데이터 저장할 경로/total_price.xlsx')
+# startDate = 시작 날짜
+# endDate = 마지막 날짜
+# timeframe = day: 일자별 / week: 주별 / month: 월별
+# number = 연습용으로 앞에 몇개 종목만 실험하고 싶을 때 숫자를 넣을 수 있음
+
+price_result = get_price_data(path, '20210805','20210807', timeframe='day', number = 4)
+
+
+price_result.to_excel(r'/Users/가격데이터 저장할 파일 경로.xlsx')
 
